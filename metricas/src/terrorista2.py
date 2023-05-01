f20b2cc4e7d77b6d58fc39d0f4b441436960478d
jaime <jagoca@live.com>
2023-05-01 18:03:07 +0200

    cross reference added

--- metricas/src/terrorista2.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python3
-import rospy
-
-rospy.init_node('my_node', anonymous=True)
-
-# print(rospy.has_param('my_param'))
-my_param = rospy.get_param('my_node/my_param')
-print("The value of my_param is:", my_param)
